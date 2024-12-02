dcfc9fd031e104bc726e6bbccaeace633b3ad84f
Luciano Belotto <DarthLuch@gmail.com>
2024-12-02 11:14:38 -0500

    2024-12-02 - add rest

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,3 +1,5 @@
+import com.example.myapp.AuthorityTest
+
 // Added by the Spring Security Core plugin:
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.example.myapp.User'
 grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'com.example.myapp.UserRole'
@@ -7,27 +9,27 @@ grails.plugin.springsecurity.rejectIfNoRule = true
 grails.plugin.springsecurity.securityConfigType = 'InterceptUrlMap'
 
 grails.plugin.springsecurity.interceptUrlMap = [
-	[pattern: '/',               		access: ['permitAll']],
-	[pattern: '/error',          		access: ['permitAll']],
-	[pattern: '/index',          		access: ['permitAll']],
-	[pattern: '/index.gsp',      		access: ['permitAll']],
-	[pattern: '/public/**',      		access: ['permitAll']],
-	[pattern: '/shutdown',       		access: ['permitAll']],
-	[pattern: '/assets/**',      		access: ['permitAll']],
-	[pattern: '/**/js/**',       		access: ['permitAll']],
-	[pattern: '/**/css/**',      		access: ['permitAll']],
-	[pattern: '/**/images/**',   		access: ['permitAll']],
-	[pattern: '/**/favicon.ico', 		access: ['permitAll']],
-	[pattern: '/api/login',				access: ['ROLE_ANONYMOUS']],
-	[pattern: '/oauth/access_token',	access: ['ROLE_ANONYMOUS']],
-	[pattern: '/secure',				access: ['hasAnyAuthority("ROLE_ADMIN")']],
+		[pattern: '/',               		access: ['permitAll']],
+		[pattern: '/error',          		access: ['permitAll']],
+		[pattern: '/index',          		access: ['permitAll']],
+		[pattern: '/index.gsp',      		access: ['permitAll']],
+		[pattern: '/public/**',      		access: ['permitAll']],
+		[pattern: '/shutdown',       		access: ['permitAll']],
+		[pattern: '/assets/**',      		access: ['permitAll']],
+		[pattern: '/**/js/**',       		access: ['permitAll']],
+		[pattern: '/**/css/**',      		access: ['permitAll']],
+		[pattern: '/**/images/**',   		access: ['permitAll']],
+		[pattern: '/**/favicon.ico', 		access: ['permitAll']],
+		[pattern: '/api/login',				access: ['ROLE_ANONYMOUS']],
+		[pattern: '/oauth/access_token',	access: ['ROLE_ANONYMOUS']],
+		[pattern: '/secure', 				access: ["hasAnyAuthority(${AuthorityTest.getCommaSeparatedAuthorities()})"]],
 ]
 
 grails.plugin.springsecurity.filterChain.chainMap = [
-	[pattern: '/assets/**',      filters: 'none'],
-	[pattern: '/**/js/**',       filters: 'none'],
-	[pattern: '/**/css/**',      filters: 'none'],
-	[pattern: '/**/images/**',   filters: 'none'],
-	[pattern: '/**/favicon.ico', filters: 'none'],
-	[pattern: '/**',             filters: 'JOINED_FILTERS']
+		[pattern: '/assets/**',      filters: 'none'],
+		[pattern: '/**/js/**',       filters: 'none'],
+		[pattern: '/**/css/**',      filters: 'none'],
+		[pattern: '/**/images/**',   filters: 'none'],
+		[pattern: '/**/favicon.ico', filters: 'none'],
+		[pattern: '/**',             filters: 'JOINED_FILTERS']
 ]
